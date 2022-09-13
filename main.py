4fe3f6624c5f1c5909b000678740f06b7b3b12fd
somedev111 <113191801+somedev111@users.noreply.github.com>
2022-09-13 13:19:12 +0100

    refactor code

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import os
 import textwrap
 import logging
 from time import sleep
+from dotenv import load_dotenv
 
 import telegram
+
+
+logger = logging.getLogger('bot')
 
 
 class TelegramLogsHandler(logging.Handler):
@@ -20,7 +24,67 @@ class TelegramLogsHandler(logging.Handler):
 
 
 def start_bot(tg_bot, chat_id, dvmn_token):
-    logger = logging.getLogger('bot')
+
+    logger.warning('Бот запущен.')
+
+    headers = {"Authorization": dvmn_token}
+
+    url = "https://dvmn.org/api/long_polling/"
+    while True:
+        try:
+            payload = {}
+            response = requests.get(url, headers=headers, params=payload)
+
+            reviews = response.json()
+
+            status = reviews.get('status')
+
+            if status == 'timeout':
+                timestamp_to_request = reviews.get('timestamp_to_request')
+                payload['timestamp'] = timestamp_to_request
+
+            if status == 'found':
+                last_attempt_timestamp = reviews.get('last_attempt_timestamp')
+                payload['timestamp'] = last_attempt_timestamp
+
+                new_attempts = reviews.get('new_attempts')
+                if new_attempts:
+                    msg_text = "Проверенные работы:\n\n"
+
+                    for attempt in new_attempts:
+                        result = "Урок пройден!"
+                        if attempt.get('is_negative'):
+                            result = f"Есть доработки"
+
+                        msg_text += f"""\
+                        Урок: {attempt.get('lesson_title')}
+                        Результат проверки: {result}
+                        Ссылка на урок: {attempt.get('lesson_url')}
+                        """
+                        msg_text += "\n\n"
+
+                    tg_bot.send_message(text=textwrap.dedent(msg_text), chat_id=chat_id)
+
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            logger.critical('ConnectionError')
+            sleep(7200)
+            continue
+        except Exception as err:
+            logger.warning('Бот упал с ошибкой:')
+            logger.exception(err)
+            break
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    tg_token = os.getenv("TG_TOKEN")
+    chat_id = os.getenv("TG_CHAT_ID")
+    dvmn_token = os.getenv("DVMN_TOKEN")
+
+    tg_bot = telegram.Bot(tg_token)
 
     tg_logs_handler = TelegramLogsHandler(tg_bot, chat_id)
     tg_logs_handler.setLevel(logging.WARNING)
@@ -30,66 +94,4 @@ def start_bot(tg_bot, chat_id, dvmn_token):
     stream_handler.setLevel(logging.WARNING)
     logger.addHandler(stream_handler)
 
-    logger.warning('Бот запущен.')
-
-    headers = {"Authorization": dvmn_token}
-
-    url = "https://dvmn.org/api/long_polling/"
-    try:
-        while True:
-            try:
-                payload = {}
-                response = requests.get(url, headers=headers, params=payload)
-
-                reviews = response.json()
-
-                status = reviews.get('status')
-
-                if status == 'timeout':
-                    timestamp_to_request = reviews.get('timestamp_to_request')
-                    payload['timestamp'] = timestamp_to_request
-
-                if status == 'found':
-                    last_attempt_timestamp = reviews.get('last_attempt_timestamp')
-                    payload['timestamp'] = last_attempt_timestamp
-
-                    new_attempts = reviews.get('new_attempts')
-                    if new_attempts:
-                        msg_text = "Проверенные работы:\n\n"
-
-                        for attempt in new_attempts:
-                            result = "Урок пройден!"
-                            if attempt.get('is_negative'):
-                                result = f"Есть доработки"
-
-                            msg_text += f"""\
-                            Урок: {attempt.get('lesson_title')}
-                            Результат проверки: {result}
-                            Ссылка на урок: {attempt.get('lesson_url')}
-                            """
-                            msg_text += "\n\n"
-
-                        tg_bot.send_message(text=textwrap.dedent(msg_text), chat_id=chat_id)
-
-            except requests.exceptions.ReadTimeout:
-                continue
-            except requests.exceptions.ConnectionError:
-                logger.critical('ConnectionError')
-                sleep(7200)
-                continue
-    except Exception as err:
-        logger.warning('Бот упал с ошибкой:')
-        logger.error(err, exc_info=True)
-
-
-if __name__ == '__main__':
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    tg_token = os.getenv("TG_TOKEN")
-    chat_id = os.getenv("TG_CHAT_ID")
-    dvmn_token = os.getenv("DVMN_TOKEN")
-
-    tg_bot = telegram.Bot(tg_token)
     start_bot(tg_bot, chat_id, dvmn_token)
-
